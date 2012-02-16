82802ef2e05a4b32a88e6cf21fc5c153b8ae41f6
Eric Garside <eric@knewton.com>
2012-02-16 18:18:30 -0500

    Ending-first with sentence cap markov chains
    A much better generator that should no longer leave dangling articles.

--- markov.py
+++ markov.py
@@ -5,12 +5,18 @@ from sys import exit, stdout
 from re import split, compile
 from random import choice
 from operator import itemgetter
+from nltk.data import load
+from nltk import word_tokenize
 
-# When generating for twitter, this is the minimum number of characters a
-# sentence must reach before the algorithm begins selecting smaller words to
-# force a sentence ending
-TWITTER_ENDING_MIN=100
-TWITTER_ENDING_MAX=135
+# When generating text, attempt to end a sentence when the length of the
+# sentence is within this number of the specified target size
+SENTENCE_ENDING_MIN=5
+
+# The minimum number of words for a full sentence
+SENTENCE_LENGTH_MIN=8
+
+# The total number of sentences to unsuccessfully generate before abandoning
+HARD_STOP = 50
 
 def argparser():
 	desc = "Generates pseudo-random sentences using a body of work."
@@ -37,83 +43,74 @@ def argparser():
 	return parser
 
 def srcparse(src):
-	punctuation = compile(r'[*.?!,":;-]')
-	word_list = split('\s+', punctuation.sub("", src).lower())
-	word_endings = {}
+	tokenizer = load("tokenizers/punkt/english.pickle")
+	sentences = tokenizer.tokenize(src.strip().lower())
+	rm = compile(r'[*.?!,\'":;\(\)<>]')
+	sp = compile(r'[\-\+]')
 
-	if len(word_list) < 3:
-		print "Source material must contain at least %s words" % group_size
-		exit(1)
+	starts, joins, ends = [], {}, []
 
-	for i in range(len(word_list) - 2):
-		w1 = word_list[i]
-		w2 = word_list[i + 1]
-		w3 = word_list[i + 2]
-		key = (w1, w2)
+	for sentence in sentences:
+		# Format the sentence
+		wlist = word_tokenize(sp.sub(" ", rm.sub("",
+			sentence.replace("\n", " "))))
 
-		if 0 in [len(w1), len(w2), len(w3)]:
+		if len(wlist) < 3:
+			# Ignore sentences without triples in the corpus
 			continue
 
-		# Generate doubles
-		if w1 in word_endings:
-			word_endings[w1].append(w2)
-		else:
-			word_endings[w1] = [w2]
+		# Add the sentence starting word
+		starts.append(wlist[0])
 
-		# Generate triples
-		if key in word_endings:
-			word_endings[key].append(w3)
-		else:
-			word_endings[key] = [w3]
+		# Inverse the list so we can build from the ending
+		wlist.reverse()
 
-	return word_list, word_endings
+		for i in range(len(wlist) - 2):
+			w1 = wlist[i]
+			w2 = wlist[i + 1]
+			w3 = wlist[i + 2]
+
+			# Handle zero-length breaks in the corpus.
+			if 0 in [len(w1), len(w2), len(w3)]:
+				continue
+
+			# Generate a list of words which can start a sentence properly
+			if i == 0:
+				ends.append(w1)
+
+			# Store doubles
+			try:
+				joins[w1].append(w2)
+			except KeyError:
+				joins[w1] = [w2]
+
+			# Store triples
+			key = (w1, w2)
+			try:
+				joins[key].append(w3)
+			except KeyError:
+				joins[key] = [w3]
+
+	return starts, joins, ends
 
 def punctuate(sentence, capitalize=True):
 	text = " ".join(sentence)
 	if capitalize:
 		text = text.capitalize()
-	return text + choice([".", "?", "!"])
+	return text + "."
 
-def twitter_choice(key, endings, text_length):
-	"""Filter the word list by length to target a specific character count
-
-	Args:
-		key: The key to use for selecting the ending
-		endings: A list of selected word endings
-		text_length: The current length of the generated text
-
-	Returns:
-		The next word to add to the generated output
-	"""
-	words = endings[key]
-	choose_from = []
-
-	# -1 for the space
-	remainder = 140 - text_length - 1
-
-	if len(words) == 1 and (remainder - len(words[0]) - 1) <= 140:
-		return words[0]
-
-	for word in words:
-		# Subtract an additional 1 for the punctuation
-		if remainder - len(words[0]) - 1 - 1 <= 140:
-			choose_from.append(word)
-
-	if len(choose_from) == 0:
-		return False
-
-	return choice(choose_from)
-
-def generate(words, endings, sentences=10, sentence_size=25,
+def generate(starts, joins, ends,
+				sentences=10, size=25,
 				seed=None, include_seed=False,
 				for_twitter=False, mention=None, trending=None):
 	"""Generates test using the provided words and endings.
 
 	Args:
-		words: A list of words
-		endings: A dictionary of word endings created by parsesrc()
+		starts: A list of words that starts sentences
+		joins: A dictionary of word endings (going backwards)
+		ends: A list of words that ends sentences
 		sentences: The number of sentences to generate
-		sentence_size: The approximate number of words per sentence
+		size: The approximate number of words per sentence
 		seed: A word to seed each sentence
 		include_seed: Include the seed word as the first word in the sentence
 		for_twitter: Generate output for twitter, limiting output at 140 chars
@@ -123,105 +120,65 @@ def generate(words, endings, sentences=10, sentence_size=25,
 	Return:
 		The generated output
 	"""
-	# Text length is 1 for the punctuation
-	output, sentence, iterations, text_length = [], [], 0, 1
-	trends, trending_topics, trending_topic = [], {}, None
-	w1, w2 = None, None
+
+	output, hard_stop_iterations, trending_topic = [], 0, None
 
 	if trending is not None:
-		rmhash = compile(r'#')
-		for topic in split('\n', trending.read()):
-			for w in split('\s+', rmhash.sub("", topic)):
-				if len(w) == 0:
-					continue
-				if w in words:
-					trends.append(w)
-					try:
-						trending_topics[w].append(topic)
-					except KeyError:
-						trending_topics[w] = [topic]
-		if len(trends) > 0:
-			spaces = compile(r'\s+')
-			seed = choice(trends)
-			trending_topic = "#{}".format(
-				spaces.sub("", choice(trending_topics[seed]))).lower()
-			text_length += len(trending_topic) + 1
-			output.append(trending_topic)
-
-	if mention is not None:
-		mention = "@{0}".format(mention)
-		# Plus 1 for the space
-		text_length += len(mention) + 1
+		rmformat = compile(r'#|\s+')
+		topic = choice(split('\n', trending.read()))
+		trending_topic = "#{}".format(rmformat.sub("", topic).lower())
 
 	while sentences > 0:
-		end_sentence = False
-		reset_sentence = False
-		skip_append = False
+		if hard_stop_iterations >= HARD_STOP:
+			break
 
-		if w1 is None:
-			if seed is not None and seed in words:
-				if include_seed:
-					w1 = seed
-				else:
-					w1 = choice(endings[seed])
+		# Build the sentence in reverse, starting at the ending word
+		sentence = []
+
+		# We add the mention first because the sentence
+
+		if seed in ends:
+			if include_seed:
+				w1 = seed
 			else:
-				w1 = choice(words)
-			w2 = choice(endings[w1])
-
-		# Plus 1 for the space
-		text_length += len(w1) + 1
-		sentence.append(w1)
-
-		key = (w1, w2)
-
-		iterations += 1
-
-		if key in endings:
-			if iterations >= sentence_size and len(endings[key]) == 1:
-				end_sentence = True
-				key = w1
-			else:
-				w1 = w2
-			if for_twitter and text_length >= TWITTER_ENDING_MIN:
-				# For twitter, attempt to pick compact words past 100 chars
-				w2 = twitter_choice(key, endings, text_length)
-				if w2 == False or text_length + 1 + len(w2) > 140:
-					# We must abort and retry; the sentence was too long
-					reset_sentence = True
-				else:
-					text_length += 1 + len(w2)
-					if text_length >= TWITTER_ENDING_MAX:
-						end_sentence = True
-			else:
-				w2 = choice(endings[key])
+				w1 = choice(joins[seed])
 		else:
-			end_sentence = True
+			w1 = choice(ends)
+		w2 = choice(joins[w1])
 
-		if end_sentence:
-			if w2 is not None:
+		while True:
+			sentence.append(w1)
+			key = (w1, w2)
+			if key in joins:
+				w1, w2 = w2, choice(joins[key])
+			else:
+				break
+			if w2 in starts and (len(sentence) + SENTENCE_ENDING_MIN) >= size:
 				sentence.append(w2)
-			output.append(punctuate(sentence, trending_topic is None))
-			reset_sentence = True
+				break
+
+		if len(sentence) >= SENTENCE_LENGTH_MIN:
+			if trending_topic is not None:
+				sentence.append(trending_topic)
+			sentence.reverse()
+			formatted_sentence = punctuate(sentence)
+			if mention is not None:
+				formatted_sentence += " @{}".format(mention)
+			if for_twitter:
+				if len(formatted_sentence) > 140:
+					break
+			output.append(formatted_sentence)
 			sentences -= 1
 
-		if reset_sentence:
-			if not end_sentence:
-				has_trending = False
-			w1, w2, sentence, iterations, text_length = None, None, [], 0, 1
-			if mention is not None:
-				text_length += len(mention) + 1
-			if trending_topic is not None:
-				text_length += len(trending_topic) + 1
-
-	if mention is not None:
-		output.append(mention)
+		hard_stop_iterations += 1
 
 	return " ".join(output)
 
 def main():
 	args = argparser().parse_args()
-	words, endings = srcparse(args.input.read())
-	text = generate(words, endings, args.length, args.size,
+	starts, joins, ends = srcparse(args.input.read())
+	text = generate(starts, joins, ends,
+					args.length, args.size,
 					args.seed, args.include_seed,
 					args.for_twitter, args.mention, args.trending)
 	args.output.write(text + "\n")
